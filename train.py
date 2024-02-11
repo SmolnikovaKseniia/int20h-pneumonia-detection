076790c046a32ee458484190bfa9bc4baf2d46d2
SmolnikovaKseniia <151660460+SmolnikovaKseniia@users.noreply.github.com>
2024-02-11 23:51:19 +0200

    Update train.py

--- train.py
+++ train.py
@@ -274,7 +274,7 @@ train_images = '/kaggle/input/rsna-pneumonia-detection-challenge/stage_2_train_i
 train_gen = Generator(train_images, training_filenames, pneumonia_locations, batch_size=32, image_size=256, shuffle=True, augment=True, predict=False)
 valid_gen = Generator(train_images, validation_filenames, pneumonia_locations, batch_size=32, image_size=256, shuffle=False, predict=False)
 
-model = model.fit_generator(train_gen, validation_data=valid_gen, callbacks=[learning_rate], epochs=15, workers=4, use_multiprocessing=True)
+model = model.fit_generator(train_gen, validation_data=valid_gen, callbacks=[learning_rate], epochs=2, workers=4, use_multiprocessing=True)
 
 save_model(model, 'model.h5')
 model.save_weights('model_weights.h5')
