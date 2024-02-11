086eda34cbdd1b2c941da2ce41b559a1ea35b9a1
SmolnikovaKseniia <151660460+SmolnikovaKseniia@users.noreply.github.com>
2024-02-11 20:26:58 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,11 +1,12 @@
+from tensorflow.keras.models import load_model
 test_images = '/kaggle/input/rsna-pneumonia-detection-challenge/stage_2_test_images'
 test_filenames = os.listdir(test_images)
 print('n test samples:', len(test_filenames))
 
 test_gen = Generator(test_images, test_filenames, None, batch_size=25, image_size=256, shuffle=False, predict=True)
-
-
 submission_dict = {}
+model = load_model('my_model.h5')
+
 for imgs, filenames in test_gen:
     preds = model.predict(imgs)
     for pred, filename in zip(preds, filenames):
